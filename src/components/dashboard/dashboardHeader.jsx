8e5092ab38978a7869a67b7f9b84cac4e027b73a
shahbaz <shahbaznadeem654@gmail.com>
2025-04-10 18:00:27 +0500

    start working on dashboard/setting page

--- src/components/dashboard/dashboardHeader.jsx
+++ src/components/dashboard/dashboardHeader.jsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
 import React from 'react'
+import { FaRegBell, FaUser } from "react-icons/fa";
+import dashboardUserImg from "@/images/dashboard/dashboardUserImg.png"
+import Link from 'next/link';
 
-const dashboardHeader = () => {
-  return (
-    <div>dashboardHeader</div>
-  )
+const DashboardHeader = () => {
+    return (
+        <div className='py-5 md:py-10 flex justify-between items-start flex-wrap'>
+            <div className='flex flex-col gap-5'>
+                <h3>Welcome, Amanda</h3>
+                <p>Tue, 07 June 2022</p>
+            </div>
+            <div className='flex gap-5'>
+                <Link href="/dashboard/setting" className='bg-[#FFFFFF] rounded-lg textColor p-4'>
+                    <FaRegBell size={25} />
+                </Link>
+                <span className='w-[50px] h-[50px]'>
+                    <figure classname=''>
+                        {/* <FaUser size={25} /> */}
+                        <Image src={dashboardUserImg} classname='w-full h-auto object-cover rounded' />
+                    </figure>
+                </span>
+            </div>
+        </div>
+    )
 }
 
-export default dashboardHeader
+export default DashboardHeader
--- src/pages/dashboard/setting/index.jsx
+++ src/pages/dashboard/setting/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from "@/components/dashboard/Sidebar";
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
 
 const index = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggled((prev) => !prev);
+  };
   return (
     <div className='container'>
       <div className="flex">
         <Sidebar />
-        <div className="flex-1 p-10">
-          <div className='bg-white p-10 border'>
-            <h2>Change Password</h2>
-            <div className='flex flex-wrap gap-8'>
+        <div className="flex-1 ps-10 flex flex-col gap-1">
+          <DashboardHeader />
+          <div className='bg-white p-10'>
+            <div className='flex justify-between'>
+              <h2>Change Password</h2>
+              <button>Save</button>
+            </div>
+            <div className='flex flex-wrap gap-8 mt-10'>
               <div className="w-[47%]">
                 <label className="block text-gray-800 font-medium mb-2">Current Password</label>
                 <input
@@ -19,7 +29,7 @@ const index = () => {
                 />
               </div>
               <div className="w-[47%] ">
-                <label className="block text-gray-800 font-medium mb-2">Current Password</label>
+                <label className="block text-gray-800 font-medium mb-2">Re-Enter Password</label>
                 <input
                   type="password"
                   placeholder="Enter Current Password"
@@ -27,7 +37,7 @@ const index = () => {
                 />
               </div>
               <div className="w-[47%]">
-                <label className="block text-gray-800 font-medium mb-2">Current Password</label>
+                <label className="block text-gray-800 font-medium mb-2">New Password</label>
                 <input
                   type="password"
                   placeholder="Enter Current Password"
@@ -37,9 +47,14 @@ const index = () => {
             </div>
           </div>
 
-          <div className='flex justify-center items-center'>
-            Notification
-            <span>toogle</span>
+          <div className='flex justify-between items-center bg-white p-10'>
+            <h3>Notification</h3>
+            <span onClick={handleToggle}
+              className={`w-16 h-8 flex items-center px-1 rounded-full transition-colors duration-300 ${isToggled ? "bg-blue-400" : "bg-gray-300"
+                }`}>
+              <div className={`w-6 h-6 rounded-full bg-white shadow-md transform transition-transform duration-300 ${isToggled ? "translate-x-8" : ""
+                }`} />
+            </span>
           </div>
         </div>
       </div>
