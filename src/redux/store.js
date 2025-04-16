03ef7e3f1aaf55ae8d2bffa329d6d3820a00c3d3
shahbaz <shahbaznadeem654@gmail.com>
2025-04-16 15:18:25 +0500

    remove counter slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterSlice/counterSlice';
 import authReducer from './authSlice/AuthSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer
   }
 })
